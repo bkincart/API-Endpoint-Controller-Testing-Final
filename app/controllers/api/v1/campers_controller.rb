40e859fbd05e30d070ddbd2fb9be1dfdde3904f3
Brianna <briannakincart@gmail.com>
2017-07-13 17:31:56 -0400

    Final code from clinic

--- app/controllers/api/v1/campers_controller.rb
+++ app/controllers/api/v1/campers_controller.rb
@@ -2,4 +2,10 @@ class Api::V1::CampersController < ApplicationController
   def index
     render json: Camper.all, adapter: :json
   end
+
+  def create
+    data = JSON.parse(request.body.read)
+    new_camper = Camper.create(name: data["name"], campsite_id: data["campsite_id"])
+    render json: new_camper, adapter: :json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :campers, only: :index
+      resources :campers, only: [:index, :create]
     end
   end
 end
